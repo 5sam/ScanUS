71dca2657753a228e44335a1951e0a4880aad35e
Unknown <amyf2502@usherbrooke.ca>
2020-01-22 11:48:52 -0500

    detection point reussi

--- opencv.py
+++ opencv.py
@@ -4,29 +4,30 @@ import cv2
 
 def main():
     cap = cv2.VideoCapture(0)
-    frame = cv2.imread('D:\_Udes\S4\Projet\ScanUS\Images/bon_laser_papier.jpg', cv2.IMREAD_COLOR)
+    frame = cv2.imread('D:\_Udes\S4\Projet\ScanUS\Images/bon_laser_souris_1.jpg', cv2.IMREAD_COLOR)
 
     while (True):
-        print('hello')
         #ret, frame = cap.read()
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        #hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         # define range of blue color in HSV
         lower_blue = np.array([155,25,0])
         upper_blue = np.array([179, 255, 255])
 
         # Threshold the HSV image to get only blue colors
-        mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
+        mask = cv2.inRange(gray, 250, 255,0)
+        contours,hier  = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(frame, contours,0,[255,0,0],1)
         # Bitwise-AND mask and original image
         res = cv2.bitwise_and(frame, frame, mask=mask)
         cv2.imshow('hsv',gray)
         cv2.imshow('frame', frame)
-        cv2.imshow('mask', hsv)
-        cv2.imshow('res', res)
+        cv2.imshow('mask', mask)
+        #cv2.imshow('res', res)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
+        break
+    cv2.waitKey(0)
     cap.release()
     cv2.destroyAllWindows()
     #img = cv2.imread('D:\_Udes\S4\Projet/red_dot.jpg', cv2.IMREAD_COLOR)

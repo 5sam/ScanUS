8a6f798a0d7124b26c469a4d2be80c1668fc26be
Unknown <amyf2502@usherbrooke.ca>
2020-02-19 10:03:15 -0500

    picamera

--- camera.py
+++ camera.py
@@ -1,6 +1,10 @@
 import math
 import numpy as np
 from transform import Matrix, mult
+import cv2
+import glob
+from picamera import PiCamera
+
 
 FOV_V = 10
 FOV_H = 10
@@ -33,3 +37,53 @@ def get_camera_matrix(angle_table=0):
     cam_matrix = Matrix(pos=CAMERA_POS, angles=angles)
     result_matrix = mult([floor_matrix, cam_matrix])
     return result_matrix
+
+def find_point_in_frame(frame,show=False):
+    if show:
+        cv2.namedWindow('gray', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('gray', 400, 300)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    centers = []
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(gray, 240, 255, 0)
+    contours, hier = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for contour in contours:
+        if cv2.contourArea(contour) > 500:
+            center = getcenter(contour)
+            centers += center
+            if show:
+                cv2.circle(frame, (int(center[0][0]), int(center[0][1])), 4, (0, 0, 255), -1)
+                cv2.drawContours(frame, contour, -1, [255, 0, 0], 2)
+    if show:
+        # Bitwise-AND mask and original image
+        res = cv2.bitwise_and(frame, frame, mask=mask)
+        cv2.imshow('gray', gray)
+        cv2.imshow('frame', frame)
+        cv2.imshow('mask', mask)
+        print(centers)
+        cv2.waitKey(0)
+
+    return centers
+
+
+def take_pictures():
+    camera = PiCamera()
+    camera.resolution = (1024,768)
+    images = glob.glob('D:\_Udes\S4\Projet\ScanUS\Calibration/*.png')
+    picure_index = len(images)
+
+    while (True):
+        camera.start_prewiew()
+        key = input()
+        camera.capture('image_' + str(picure_index) + '.png')
+        picure_index += 1
+        if key == 'q':
+            break
+
+
+def getcenter(contour):
+    M = cv2.moments(contour)
+    center_x = M['m10'] / M['m00']
+    center_y = M['m01'] / M['m00']
+    return [[center_x,center_y]]
---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import camera
+from calibration import calib_camera
+from transform import intersect
+import cv2
+import math
+
+def main():
+    1+1
+
+if __name__ == "__main__":
+    #main()
+    camera.take_pictures()
+    #calib_camera()
+    #line = get_camera_line()
+    #print(intersect(point_1 = line.get_pos(),vecteur_1=line.get_vector_in_referential([0,1,0]),point_2 = [1,0,0],vecteur_2 = [-1,12,-3]))

c7606740944d41c4d6bfea8b9bfbf27165320434
Unknown <amyf2502@usherbrooke.ca>
2020-02-21 15:18:31 -0500

    better plot

--- main.py
+++ main.py
@@ -10,18 +10,17 @@ import math
 def main():
     points_3d = []
     size = 10
-    ax = plot.init()
+    plot.init()
     for i in range(size):
         for j in range(size):
             for k in range(size):
                 points_3d += [[i,j, k]]
-            plot.plot(ax,points_3d)
+            plot.plot(points_3d)
             points_3d = []
 
-    input()
 
 
-    plot.plot(ax,points_3d)
+
 
 
 if __name__ == "__main__":
--- plot.py
+++ plot.py
@@ -2,14 +2,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def plot(ax,points_3D):
-
+def plot(points_3D):
+    ax = plt.gca()
     for element in points_3D:
         x = element[0]
         y = element[1]
         z = element[2]
         ax.scatter(x, y, z)
-
+    plt.pause(1/1000)
 
 def init():
     plt.ion()
@@ -18,4 +18,3 @@ def init():
     ax.set_xlim(-11, 11)
     ax.set_ylim(-11, 11)
     ax.set_zlim(-11, 11)
-    return ax
